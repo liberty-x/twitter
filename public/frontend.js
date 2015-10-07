edbb9e9807d398b32b3c7cd3cf20ff5d21ca833b
Ruth <emailtoruth@virginmedia.com>
2015-10-07 21:39:29 +0100

    starting adding to redis

--- public/frontend.js
+++ public/frontend.js
@@ -1,5 +1,21 @@
-function preventingShit(){
+var date = new Date();
+
+// function preventingShit(){
   document.getElementById('form').addEventListener('submit', function(e){
     e.preventDefault();
+
+  // function sendInputToServer(){
+    var username = document.getElementById('username').value;
+    var tweet = document.getElementById('tweet').value;
+    var out = new XMLHttpRequest();
+    out.onreadystatechange = function(){
+      if (out.readyState === 4 && out.status === 200){
+        //console.log(out.responseText);
+        document.getElementById('display').innerHTML = out.responseText;
+      }
+    };
+    out.open('POST', '/' + username + '/' + date + '/' + tweet);
+    out.send();
+  // }
   })
-}
+// }
--- server.js
+++ server.js
@@ -2,13 +2,15 @@ var http = require('http');
 var fs = require('fs');
 var port = process.env.PORT || 8000;
 console.log("Server running at http://localhost:" + port);
-// var redis  = require("redis");
-// var client = redis.createClient();
+var server = http.createServer(handler).listen(port);
+var redis  = require("redis");
+var client = redis.createClient();
 
 var index = fs.readFileSync(__dirname + '/public/index.html');
 
 function handler(req,res){
   console.log(req.method);
+  console.log(req.url);
   // function loadingPage(req,res){
     if (req.url === '/') {
       res.writeHead(200, {"Content-Type": "text/html"});
@@ -19,8 +21,15 @@ function handler(req,res){
       res.writeHead(200,{"Content-Type": "text/" + ext[1]});
       res.end(fs.readFileSync(__dirname + req.url));
     }
-}
+    else if (req.url.indexOf('%') > -1) {
+      var dataInputs = req.url.split('/');
+      console.log(dataInputs)
+      client.ZADD(dataInputs[1], dataInputs[2], dataInputs[3])
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.end();
+    }
   // }
+}
   // function sendToDB(req, callback){
   //   if (req.method === 'POST') {
   //     var body = '';
@@ -28,11 +37,11 @@ function handler(req,res){
   //         body += dataChunk;
   //     });
   //     req.on('end', function () {
-  //         });
+  //
   //     });
+  //   });
 
 
 
-var server = http.createServer(handler).listen(port);
 
 module.exports = handler;
